2c5b64c5733f3e003917fa3a018cd14cb2d81523
zengchanghuan <zengchanghuan@gmail.com>
2021-05-06 21:52:37 +0800

    add project

---
+++ kvcAndkvoTest/kvcAndkvoTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  kvcAndkvoTest
+//
+//  Created by zengchanghuan on 2021/5/6.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
